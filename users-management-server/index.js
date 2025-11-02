831ad4f58228e926775afe2ac7bacae958a77774
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2025-11-02 16:47:11 +0600

    Add users-management-server files properly

---
+++ users-management-server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("users server is available");
+});
+
+const users = [
+  { id: 1, name: "Sakib", email: "sakib@alhasan.com" },
+  { id: 2, name: "Rakib", email: "rakib@alhasan.com" },
+  { id: 3, name: "Akib", email: "akib@alhasan.com" },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post method called", req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
+});
+
+app.listen(port, () => {
+  console.log(`Users Server Started on Port: ${port}`);
+});
